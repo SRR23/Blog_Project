1c4a485301341aff6e2fcbec04486a67e71a4489
SRR23 <srrimon267@gmail.com>
2024-03-25 23:10:11 +0600

    added

--- Blog/main/forms.py
+++ Blog/main/forms.py
@@ -1,14 +1,15 @@
 from django import forms
 
 from .models import Blog
+from ckeditor.fields import RichTextField
 
 class TextForm(forms.Form):
     text = forms.CharField(widget=forms.Textarea, required=True)
+    rating = forms.ChoiceField(label="Rating", choices=[(i, str(i)) for i in range(1, 6)], required=False)
 
 
 class AddBlogForm(forms.ModelForm):
-    # description = RichTextField()
-    description = forms.Textarea()
+    description = RichTextField()
     
     class Meta:
         model = Blog
