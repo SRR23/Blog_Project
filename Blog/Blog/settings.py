332edfd68e2f8a45cb272d814af9f7aee558da7e
SRR23 <srrimon267@gmail.com>
2024-03-25 13:36:36 +0600

    added

--- Blog/Blog/settings.py
+++ Blog/Blog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d9mtlb30hk(4v#y5j!mi$b3z5c+b=)+azii6afvbuy2v4sl677'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
